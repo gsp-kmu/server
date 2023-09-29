bacd0e47e638f80f2c67d61fe103dbe2056cbb1a
mclub4 <mclub901@gmail.com>
2023-09-29 16:35:20 +0900

    카드 랜덤 뽑기 기능

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const MainService = require('./src/MainController');
+const RandomService = require('./src/shop/randomCard');
 const app = express();
 
 const mainService = new MainService();
+const randomService = new RandomService();
 mainService.Start();
 
 app.set('port', 8000);
-app.use('/', (req, res) => {
+
+app.use('/random', (req, res) => {
+    res.send(randomService.Start());
+});
+
+app.use('/test', (req, res) => {
     mainService.JoinUser('강현서', '20191546');
     res.send('test');
 });
