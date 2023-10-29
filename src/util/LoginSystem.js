2a9e7758ef2cf1e4b8acb59781bc6946ae788b37
mclub4 <mclub901@gmail.com>
2023-10-30 00:49:24 +0900

    로그인 오류 수정

--- src/util/LoginSystem.js
+++ src/util/LoginSystem.js
@@ -32,22 +32,34 @@ class LoginSystem{
         }
     }
 
-    async Login(id, pw){
-        const cryptedId = await cryptoModule.cipher(this._id);
+    async Login(){
+        const id = await cryptoModule.cipher(this._id);
         const comparePW = new Promise(async (resolve, reject) =>{
         const salt = await Account.findOne({
             attributes: ['salt'],
             raw: true,
             where:{
-                cryptedId,
+                id,
             },
         },);
-        crypto.pbkdf2(pw, salt, 104906, 64, 'sha512', (err, key) => {
+        console.log(salt.salt);
+        crypto.pbkdf2(this._password, salt.salt, 104906, 64, 'sha512', (err, key) => {
             if(err) rejects(err);
             else resolve({hashed : key.toString('base64'), salt});
         });
-    },).then(async (result) => {
-        if(this._password == result.hashed){
+        },)
+        .then(async (result) => {
+        console.log(result.hashed);
+        const realPW = await Account.findOne({
+            attributes: ['password'],
+            raw: true,
+            where:{
+                id,
+            },
+        },);
+        console.log("해쉬 돌려본 값 " + result.hashed);
+        console.log("DB에 있는 값 " + realPW.password);
+        if(realPW.password == result.hashed){
             console.log("로그인 성공!");
             return true;
         }
