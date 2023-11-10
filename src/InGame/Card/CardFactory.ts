897a465cddc8a499b10ebc605fbca3e4e7ce815b
hyeonseokang <hansx2079@naver.com>
2023-11-10 09:35:54 +0900

    Card 순서 변경

--- src/InGame/Card/CardFactory.ts
+++ src/InGame/Card/CardFactory.ts
@@ -19,19 +19,25 @@ export class CardFactory {
         //if(cardId == 3)
             //return new SolCard(userId, 3, data);
         if (cardId == 1)
-            return new Card(userId, 0, data);
+            return new ReaperCard(userId, 0, data, 11);
         else if(cardId == 2)
-            return new Card(userId, 1, data);
+            return new HackerCard(userId, 1, data);
         else if(cardId == 3)
-            return new Card(userId, 2, data);
+            return new Aico(userId, 2, data, cardId);
         else if(cardId == 4)
             return new SolCard(userId, 3, data);
         else if (cardId == 5)
-            return new Card(userId, 4, data);
+            return new NecromancerCard(userId, 4, data);
         else if(cardId == 6)
             return new BlackAicoCard(userId, 5, data, cardId);
         else if(cardId == 7)
-            return new Card(userId, 6, data);
+            return new FallenAngelCard(userId, 6, data);
+        else if(cardId == 8)
+            return new MedusaCard(userId, 7, data);
+        else if(cardId == 9)
+            return new ElfCard(userId, 8, data);
+        else if(cardId ==10)
+            return new LunaCard(userId, 9, data);
         else if(cardId == 8)
             return new Card(userId, 7, data);
         else if(cardId == 9)
@@ -39,25 +45,19 @@ export class CardFactory {
         else if(cardId == 10)
             return new Card(userId, 9, data);
         else if (cardId == 11)
-            return new ReaperCard(userId, 0, data, 11);
+            return new Card(userId, 0, data);
         else if(cardId == 12)
-            return new HackerCard(userId, 2, data);
+            return new Card(userId, 1, data);
         else if(cardId == 13)
-            return new Aico(userId, 2, data, cardId);
+            return new Card(userId, 2, data);
         else if(cardId == 14)
             return new Card(userId, 3, data);
         else if (cardId == 15)
-            return new NecromancerCard(userId, 4, data);
+            return new Card(userId, 4, data);
         else if(cardId == 16)
             return new Card(userId, 5, data);
         else if(cardId == 17)
-            return new FallenAngelCard(userId, 6, data);
-        else if(cardId == 18)
-            return new MedusaCard(userId, 7, data);
-        else if(cardId == 19)
-            return new ElfCard(userId, 8, data);
-        else if(cardId == 20)
-            return new LunaCard(userId, 9, data);
+            return new Card(userId, 6, data);
 
         return new Card(userId, 5, data);
     }
