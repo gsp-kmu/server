a2d2055db4531dcef17b5df438392ed714bd2eae
mclub4 <mclub4@kookmin.ac.kr>
2023-11-11 13:25:22 +0900

    메시지 수정

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/random', async (req, res) => {
     }
     catch (err){
         console.log(err);
-        res.status(400).send("Failed");
+        res.status(400).json({msg : "Unexpected Error"});
     }
 });
 
@@ -58,7 +58,7 @@ app.post("/register", async (req, res) => {
     }
     catch (err){
         console.log(err);
-        res.status(401).send('Format Error');
+        res.status(401).send('Unexpected Error');
     }
 })
 
@@ -103,7 +103,7 @@ app.post("/savedeck", async(req, res)=> {
     }
     catch(err){
         console.log(err);
-        res.status(402).send("Format Error");
+        res.status(402).send("Unexpected Error");
     }
 });
 
@@ -117,28 +117,28 @@ app.post("/getdeck", async(req, res)=> {
         const module = new GetDeck(userId);
         const decklist = await module.getDeckList();
 
-        res.status(200).json({msg:"All decks are updated",deckList:decklist});
+        res.status(200).json({msg:"Successfully get deck list",deckList:decklist});
     }
     catch{
-        res.status(400).json({msg:"Format Error"});
+        res.status(400).json({msg:"Unexpected Error"});
     }    
 });
 
 // 전체 카드 리스트 얻기
-app.get("/getcard", async(req, res)=> {
+app.post("/getcard", async(req, res)=> {
     try{
         const GetAllCard = require('./src/deck/GetAllCard.ts');
         const {userId} = req.body;
 
         console.log(userId + "번 유저가 전체 카드 목록을 얻을려고 시도합니다.");
         const module = new GetAllCard(userId);
-        const decklist = await module.getCardList();
+        const cardlist = await module.getCardList();
 
-        res.status(200).json({msg:"Successfully get card list",deckList:decklist});
+        res.status(200).json({msg:"Successfully get card list",cardList:cardlist});
     }
     catch (err){
         console.log(err);
-        res.status(400).json({msg:"Error"});
+        res.status(400).json({msg:"Unexpected Error"});
     }    
 });
 
